b70ccab15c1ed2d4e4d83e98151301f848731b3d
Vy <danvynguyen03@outlook.com>
2025-06-21 06:46:15 +0000

    [automatic commit] 2025-06-21 06:46:15 UTC 1.158.160.201

--- part1/routes/messages.js
+++ part1/routes/messages.js
@@ -12,7 +12,16 @@ router.get('/dogs', async(req, res) => {
     res.json(rows);
 });
 
-router.get(/walkre)
+router.get(/walkrequests/open/, async(req, res) => {
+    const [rows] = await db.query(`
+        SELECT wr.request_id, wr.walk_date, u.name AS walker_name, d.name AS dog_name
+        FROM WalkRequests wr
+        JOIN Users u ON wr.walker_id = u.user_id
+        JOIN Dogs d ON wr.dog_id = d.dog_id
+        WHERE wr.status = 'open'
+    `);
+    res.json(rows);
+}
 
 
 module.exports = router;
