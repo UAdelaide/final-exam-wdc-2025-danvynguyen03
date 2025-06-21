b7fa37a0142d4c63c8951079f38dabdd3ec4f98b
Vy <danvynguyen03@outlook.com>
2025-06-21 05:17:02 +0000

    [automatic commit] 2025-06-21 05:17:02 UTC 1.158.160.201

---
+++ part1/db.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2/promise');
+
+const db = mysql.createPool({
+  socketPath: '/var/run/mysqld/mysqld.sock',
+  host: '127.0.0.1',
+  user: 'root',
+  password: 'newpassword',
+  database: 'do'
+});
+
+module.exports = db;
