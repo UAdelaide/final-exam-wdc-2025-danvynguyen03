fb68152e84f1c873eaf27c181f9bc4842a06d5f6
Vy <danvynguyen03@outlook.com>
2025-06-21 05:27:30 +0000

    [automatic commit] 2025-06-21 05:27:30 UTC 1.158.160.201

--- part1/routes/dogs.js
+++ part1/routes/dogs.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET dog list */
 router.get('/', async(req, res) {
     const [rows] = await req.db.execute('SELECT * FROM dogs');
-    res.send('respond with a resource');
+    res.json(rows);
 });
 
 module.exports = router;
