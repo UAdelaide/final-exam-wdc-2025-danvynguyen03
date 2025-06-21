368a19fb365f7a2f9338a356a307427b6b4fd41f
Vy <danvynguyen03@outlook.com>
2025-06-21 06:03:35 +0000

    [automatic commit] 2025-06-21 06:03:35 UTC 1.158.160.201

--- part1/db.js
+++ part1/db.js
@@ -1,6 +1,6 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
-const pool = mysql.createPool({
+const db = mysql.createPool({
   host: 'localhost',
   user: 'root',
   password: 'mypassword',
