cc30c93ea3ab377894ec32ec6360313129dbc11b
Vy <danvynguyen03@outlook.com>
2025-06-21 05:59:09 +0000

    [automatic commit] 2025-06-21 05:59:09 UTC 1.158.160.201

--- part1/db.js
+++ part1/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
   socketPath: '/var/run/mysqld/mysqld.sock',
-  host: 'l',
+  host: '127.0.0.1',
   user: 'root',
   password: 'mypassword',
   database: 'DogWalkService'
