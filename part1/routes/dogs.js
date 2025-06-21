149aa868f5baf27115c485d060e7b6a858d35612
Vy <danvynguyen03@outlook.com>
2025-06-21 05:26:49 +0000

    [automatic commit] 2025-06-21 05:26:49 UTC ;; communications error to 208.67.222.222#53: timed out 1.158.160.201

--- part1/routes/dogs.js
+++ part1/routes/dogs.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET dog list */
-router.get('/dogs', async(req, res) {
-    const [riws]
+router.get('/', async(req, res) {
+    const [rows]
   res.send('respond with a resource');
 });
 
